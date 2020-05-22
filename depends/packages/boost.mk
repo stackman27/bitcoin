74a11a27d1a57cb6dc0577a6770925404ad023d8
Carl Dong <contact@carldong.me>
2020-05-22 17:00:53 -0400

    depends: boost: Split target-os from toolset
    Previously, we specified the target-os in the toolset (and sometimes
    used the wrong command line flags), now we have a clear separation,
    which is favored by ./bootstrap.sh and ./b2.
    
    This means that all supported OSes will specify the correct target-os=
    and toolset= on the command line.

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -21,9 +21,12 @@ $(package)_config_opts_x86_64_android=address-model=64
 $(package)_config_opts_armv7a_android=address-model=32
 $(package)_toolset_$(host_os)=gcc
 $(package)_toolset_darwin=clang
+<<<<<<< HEAD
 ifneq (,$(findstring clang,$($(package)_cxx)))
    $(package)_toolset_$(host_os)=clang
 endif
+=======
+>>>>>>> depends: boost: Split target-os from toolset
 $(package)_archiver_$(host_os)=$($(package)_ar)
 $(package)_config_libraries=filesystem,system,thread,test
 $(package)_cxxflags=-std=c++11 -fvisibility=hidden
