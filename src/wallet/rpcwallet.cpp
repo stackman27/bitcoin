990209b475d287fd4f32441f60e68181013324e3
Sishir Giri <sishirg27@gmail.com>
2020-06-20 15:34:13 -0700

    [test] test send* wallet rpcs with verbose = True

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -416,7 +416,7 @@ static UniValue sendtoaddress(const JSONRPCRequest& request)
                             "Whether to display the fee reason or not."},
                 },
                 RPCResult{
-                    RPCResult::Type::STR_HEX, "txid", "The transaction id.",
+                    RPCResult::Type::STR_HEX, "txid", "The transaction id.", 
                 },
                 RPCExamples{
                     HelpExampleCli("sendtoaddress", "\"" + EXAMPLE_ADDRESS[0] + "\" 0.1")
@@ -472,12 +472,13 @@ static UniValue sendtoaddress(const JSONRPCRequest& request)
 
     SetFeeEstimateMode(pwallet, coin_control, request.params[7], request.params[6]);
 
-    EnsureWalletIsUnlocked(pwallet);
+    EnsureWalletIsUnlocked(pwallet); 
     UniValue entry(UniValue::VOBJ);
     std::string feeReason;
     CTransactionRef tx = SendMoney(pwallet, dest, nAmount, fSubtractFeeFromAmount, coin_control, std::move(mapValue), feeReason);
 
     bool verbose = request.params[9].isNull() ? false : request.params[9].get_bool();
+
     if(verbose){
         entry.pushKV("hex", tx->GetHash().GetHex());
         entry.pushKV("Fee Reason", feeReason);
@@ -830,7 +831,14 @@ static UniValue sendmany(const JSONRPCRequest& request)
                 },
                  RPCResult{
                      RPCResult::Type::STR_HEX, "txid", "The transaction id for the send. Only 1 transaction is created regardless of\n"
-            "the number of addresses.", 
+            "the number of addresses.",  
+                    {"verbose", RPCArg::Type::BOOL,  /* default */ "false", 
+                            "Whether to display the fee reason or not."},
+                },
+                 RPCResult{
+                     RPCResult::Type::STR_HEX, "txid", "The transaction id for the send. Only 1 transaction is created regardless of\n"
+
+            "the number of addresses.",  
                  },
                 RPCExamples{
             "\nSend two amounts to two different addresses:\n"
@@ -923,6 +931,7 @@ static UniValue sendmany(const JSONRPCRequest& request)
     pwallet->CommitTransaction(tx, std::move(mapValue), {} /* orderForm */);
  
     bool verbose = request.params[8].isNull() ? false : request.params[8].get_bool();
+
     if(verbose){
         entry.pushKV("hex", tx->GetHash().GetHex());
         entry.pushKV("Fee Reason", feeReason);  
--- test/functional/wallet_basic.py
+++ test/functional/wallet_basic.py
@@ -87,7 +87,8 @@ class WalletTest(BitcoinTestFramework):
 
         # Send 21 BTC from 0 to 2 using sendtoaddress call.
         self.nodes[0].sendtoaddress(self.nodes[2].getnewaddress(), 11)
-        mempool_txid = self.nodes[0].sendtoaddress(self.nodes[2].getnewaddress(), 10)
+        mempool_txid = self.nodes[0].sendtoaddress(self.nodes[2].getnewaddress(), 10)  
+        
 
         self.log.info("test gettxout (second part)")
         # utxo spent in mempool should be visible if you exclude mempool
@@ -189,7 +190,7 @@ class WalletTest(BitcoinTestFramework):
         address = self.nodes[0].getnewaddress("test")
         fee_per_byte = Decimal('0.001') / 1000
         self.nodes[2].settxfee(fee_per_byte * 1000)
-        txid = self.nodes[2].sendtoaddress(address, 10, "", "", False) 
+        txid = self.nodes[2].sendtoaddress(address, 10, "", "", False)  
         self.nodes[2].generate(1)
         self.sync_all(self.nodes[0:3])
         node_2_bal = self.check_fee_amount(self.nodes[2].getbalance(), Decimal('84'), fee_per_byte, self.get_vsize(self.nodes[2].gettransaction(txid)['hex']))
@@ -201,9 +202,9 @@ class WalletTest(BitcoinTestFramework):
         node_2_bal -= Decimal('10')
         assert_equal(self.nodes[2].getbalance(), node_2_bal)
         node_0_bal = self.check_fee_amount(self.nodes[0].getbalance(), Decimal('20'), fee_per_byte, self.get_vsize(self.nodes[2].gettransaction(txid)['hex']))
-        
+
         # Sendmany 10 BTC
-        txid = self.nodes[2].sendmany('', {address: 10}, 0, "", [])  
+        txid = self.nodes[2].sendmany('', {address: 10}, 0, "", []) 
         self.nodes[2].generate(1)
         self.sync_all(self.nodes[0:3])
         node_0_bal += Decimal('10')
